f1fd70b92515db056fc6370bd5c40fc37e2dd64e
KRAK08 <ramirezalvarezkenneth08@gmail.com>
2025-03-31 19:06:35 -0600

    Add category controller and the migration to update database tables

---
+++ InventoryApp/Inventory.Api/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using Inventory.Api.UnitsOfWork.Interfaces;
+using InventoryApp.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : GenericController<Category>
+    {
+        private readonly ICategoryUnitOfWork _categoryUnitOfWork;
+
+        public CategoryController(IUnitOfWork<Category> unitOfWork, ICategoryUnitOfWork categoryUnitOfWork) : base(unitOfWork)
+        {
+            _categoryUnitOfWork = categoryUnitOfWork;
+        }
+
+        [HttpGet("{id:int}")]
+        public override async Task<IActionResult> GetAsync(int id)
+        {
+            var data = await _categoryUnitOfWork.GetAsync(id);
+            return Ok(data.Result);
+        }
+
+        [HttpGet]
+        public override async Task<IActionResult> GetAllAsync()
+        {
+            var list = await _categoryUnitOfWork.GetAllAsync();
+            return Ok(list);
+        }
+    }
+}
